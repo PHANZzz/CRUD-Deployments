4d6e34f8afb5530f2105d2137f9267a5969dbc05
Kan Sophan <ytbro65@gmail.com>
2023-07-17 17:23:41 +0700

    first

--- back-end/index.js
+++ back-end/index.js
@@ -45,6 +45,51 @@ app.get('/api/list', async (req, res) => {
   }
 });
 
+app.get('/api/get/:id', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('StudentManagementSystem');
+    const collection = db.collection('Student');
+    const result = await collection.findOne({ _id: new ObjectId(req.params.id) });
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while retrieving the document' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.put('/api/update/:id', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('StudentManagementSystem');
+    const collection = db.collection('Student');
+    const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+    res.status(200).json({ message: 'Document updated successfully', modifiedCount: result.modifiedCount });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while updating the document' });
+  } finally {
+    await client.close();
+  }
+});
+app.delete('/api/delete/:id', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('StudentManagementSystem');
+    const collection = db.collection('Student');
+    const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+    res.status(200).json({ message: 'Document deleted successfully', deletedCount: result.deletedCount });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while deleting the document' });
+  } finally {
+    await client.close();
+  }
+});
+
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
